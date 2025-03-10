7b7397481cf8afbbf03d1f59ec8825dd0be45aaf
jeetOnGit <jeetdas.in@gmail.com>
2025-03-10 20:35:44 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='text-5xl'>Hello World</h1>
+    </div> 
+  )
+}
+
+export default App
